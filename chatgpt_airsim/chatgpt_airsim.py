929ebfd7295f5c79783e8052e57c1eee4dcd175e
Sai Vemprala <saihemachandra.v@gmail.com>
2023-02-20 15:59:02 -0800

    Switch to the more widely used revchatGPT interface

--- chatgpt_airsim/chatgpt_airsim.py
+++ chatgpt_airsim/chatgpt_airsim.py
@@ -1,15 +1,47 @@
-from chatgpt_wrapper import ChatGPT
+from revChatGPT.V1 import Chatbot
 import re
-import airsim
 import argparse
 from airsim_wrapper import *
 import math
 import numpy as np
 import os
+import json
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--prompt", type=str, default="prompts/airsim_basic.txt")
 args = parser.parse_args()
+
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+print("Initializing ChatGPT...")
+chatgpt = Chatbot(config=config)
+
+
+def ask(prompt):
+    for data in chatgpt.ask(prompt):
+        response = data["message"]
+
+    return response
+
+
+print(f"Done.")
+
+code_block_regex = re.compile(r"```(.*?)```", re.DOTALL)
+
+
+def extract_python_code(content):
+    code_blocks = code_block_regex.findall(content)
+    if code_blocks:
+        full_code = "\n".join(code_blocks)
+
+        if full_code.startswith("python"):
+            full_code = full_code[7:]
+
+        return full_code
+    else:
+        return None
 
 
 class colors:  # You may need to change color settings
@@ -20,25 +52,15 @@ class colors:  # You may need to change color settings
     BLUE = "\033[34m"
 
 
-bot = ChatGPT()
+print(f"Initializing AirSim...")
 aw = AirSimWrapper()
+print(f"Done.")
 
 with open(args.prompt, "r") as f:
     prompt = f.read()
 
-bot.ask(prompt)
+ask(prompt)
 print("Welcome to the AirSim chatbot! I am ready to help you with your AirSim questions and commands.")
-
-code_block_regex = re.compile(r"```(.*?)```", re.DOTALL)
-
-
-def extract_python_code(content):
-    code_blocks = code_block_regex.findall(content)
-    if code_blocks:
-        return "\n".join(code_blocks)
-    else:
-        return None
-
 
 while True:
     question = input(colors.YELLOW + "AirSim> " + colors.ENDC)
@@ -46,17 +68,16 @@ while True:
     if question == "!quit" or question == "!exit":
         break
 
-    if question == "!cls":
+    if question == "!clear":
         os.system("cls")
         continue
 
-    response = bot.ask(question)
-    print(response)
-    print("\n")
+    response = ask(question)
+
+    print(f"\n{response}\n")
 
     code = extract_python_code(response)
     if code is not None:
         print("Please wait while I run the code in AirSim...")
         exec(extract_python_code(response))
-        print("Done!")
-        print("\n")
+        print("Done!\n")
