15e81334d03edf14ab019f14a94a2b60eb591cc3
Jin Kim <jinyoungkim927@gmail.com>
2024-01-13 12:50:55 -0800

    Edited airsim_wrapper.py

--- chatgpt_airsim/airsim_wrapper.py
+++ chatgpt_airsim/airsim_wrapper.py
@@ -138,11 +138,11 @@ class AirSimWrapper:
         self.stop_thread = True
         if self.flutter_thread is not None:
             self.flutter_thread.join()
-    
+
     def generate_circular_path(center, radius, height, segments=12):
         path = []
         for i in range(segments):
-            angle = 2 * math.pi * i/segments
+            angle = 2 * math.pi * i / segments
             x = center[0] + radius * math.cos(angle)
             y = center[1] + radius * math.sin(angle)
             z = height
@@ -226,7 +226,7 @@ class AirSimWrapper:
         # image = vision.Image(content=content)
 
         # objects = client.object_localization(image=image).localized_object_annotations
-    
+
     def query_language_model(prompt):
         openai.api_key = config["OPENAI_API_KEY"]
         chat_history = [
@@ -240,29 +240,38 @@ class AirSimWrapper:
         )
         return completion.choices[0].message.content
 
-    # Complex commands 
+    # Complex commands
     def count(self, object_name):
         filename = "count" + object_name + ".jpg"
         image_data = take_photo(filename)
         vision_outputs = analyze_with_vision_model(image_data)
         # Naive: converts vision model json output to string, append to count prompt
-        prompt = "\n Based on this json output, count the number of instances of " + object_name + " in the scene. Return a single number"
+        prompt = (
+            "\n Based on this json output, count the number of instances of "
+            + object_name
+            + " in the scene. Return a single number"
+        )
         return query_language_model(str(vision_outputs) + prompt)
-    
+
     def search(self, object_name, radius):
         # code motion
         fly_to(get_position(object_name))
         # fly in a circle
-        circular_path = generate_circular_path(get_position(object_name)[:2], radius, get_position(object_name)[2])
+        circular_path = generate_circular_path(
+            get_position(object_name)[:2], radius, get_position(object_name)[2]
+        )
         vision_outputs = []
-        for point in circular path:
+        for point in circular_path:
             fly_to(point)
             image_data = take_photo(str(point))
             vision_output = analyze_with_vision_model(image_data)
             vision_outputs.append(vision_output)
-        prompt = "\n Based on these json outputs, is " + object_name + "present in the scene? Return TRUE or FALSE."
+        prompt = (
+            "\n Based on these json outputs, is "
+            + object_name
+            + "present in the scene? Return TRUE or FALSE."
+        )
         return query_language_model(str(vision_outputs) + prompt)
-        
 
     def get_latitude_longitude(self, object_name):
         fly_to(get_position(object_name))
